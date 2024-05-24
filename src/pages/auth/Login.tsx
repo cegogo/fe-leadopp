cc4916975a57c9632cb50ec1ae97faada6fcf37e
muallayuce <muallayuce@gmail.com>
2024-05-24 11:41:09 +0200

    login with google auth and username-password

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,23 +1,52 @@
 import { useEffect, useState } from 'react';
 import { Grid, Stack, Typography, TextField, Button } from '@mui/material';
+import { useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
+import imgGoogle from '../../assets/images/auth/google.svg';
 import imgLogo from '../../assets/images/auth/img_logo.png';
 import imgLogin from '../../assets/images/auth/img_login.png';
+import { GoogleButton } from '../../styles/CssStyled';
 import { fetchData } from '../../components/FetchData';
-import { LoginUrl } from '../../services/ApiUrls';
+import { AuthUrl, LoginUrl } from '../../services/ApiUrls';
 import '../../styles/style.css';
+
+declare global {
+    interface Window {
+        google: any;
+        gapi: any;
+    }
+}
 
 export default function Login() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [token, setToken] = useState(false);
 
     useEffect(() => {
         if (localStorage.getItem('Token')) {
             navigate('/app');
         }
-    }, [navigate]);
+    }, [token, navigate]);
+
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => {
+            const apiToken = { token: tokenResponse.access_token };
+            const head = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            };
+            fetchData(`${AuthUrl}/`, 'POST', JSON.stringify(apiToken), head)
+                .then((res) => {
+                    localStorage.setItem('Token', `Bearer ${res.access_token}`);
+                    setToken(true);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        },
+    });
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -93,6 +122,12 @@ export default function Login() {
                                 </Button>
                             </form>
                         </Grid>
+                        <Grid item sx={{ mt: 4 }}>
+                            <GoogleButton variant='outlined' onClick={() => login()} sx={{ fontSize: '12px', fontWeight: 500 }}>
+                                Sign in with Google
+                                <img src={imgGoogle} alt='google' style={{ width: '17px', marginLeft: '5px' }} />
+                            </GoogleButton>
+                        </Grid>
                     </Grid>
                 </Grid>
                 <Grid
@@ -108,7 +143,7 @@ export default function Login() {
                     <Grid item >
                         <Stack sx={{ alignItems: 'center' }}>
                             <h3>Welcome to BottleCRM</h3>
-                            <p> Free and OpenSource CRM from small medium business.</p>
+                            <p> Free and OpenSource CRM for small and medium businesses.</p>
                             <img
                                 src={imgLogin}
                                 alt='register_ad_image'
