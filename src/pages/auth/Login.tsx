4191f24fc23ffdc6d602f124567f01a0e7071bb7
Nrdntrkr <nurdanturker@gmail.com>
2024-06-12 23:13:41 +0200

    auth works

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -7,7 +7,7 @@ import imgLogo from '../../assets/images/auth/img_logo.png';
 import imgLogin from '../../assets/images/auth/img_login.png';
 import { GoogleButton } from '../../styles/CssStyled';
 import { fetchData } from '../../components/FetchData';
-import { AuthUrl, LoginUrl, RegisterUrl, CheckUserCountUrl } from '../../services/ApiUrls';
+import { AuthUrl, LoginUrl, RegisterUrl, AdminUrl, CheckUserCountUrl } from '../../services/ApiUrls';
 import '../../styles/style.css';
 
 declare global {
@@ -27,6 +27,12 @@ export default function Login() {
     const [signUpEmail, setSignUpEmail] = useState('');
     const [signUpPassword, setSignUpPassword] = useState('');
     const [signUpError, setSignUpError] = useState('');
+    // const [authData, setAuthData] = useState<AuthData>({  google_authentication: false });
+    const [authData, setAuthData] = useState(false);
+
+    // interface AuthData {        
+    //     google_authentication: boolean;
+    // }
 
     useEffect(() => {
         if (localStorage.getItem('Token')) {
@@ -124,6 +130,31 @@ export default function Login() {
             });
     };
     
+    useEffect(() => {
+
+
+        const getAuth = async () => {
+            const Header = {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+
+            }
+            try {
+                await fetchData(`${AdminUrl}/`, 'GET', null as any, Header)
+                    .then((res: any) => {
+                        if (!res.error) {
+                            console.log(res.is_google_auth)
+                            setAuthData(res.is_google_auth)
+                        }
+                    })
+            }
+            catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        getAuth();
+
+}, []);
 
     return (
         <div>
@@ -177,6 +208,7 @@ export default function Login() {
                             {/* Choosing the header depend on condition */}
                             {userExists ? 'Sign In' : 'Sign Up'}
                         </Typography>
+                        {!authData && (
                         <Grid item sx={{ mt: 4 }}>
                             {/* Rendering one form and choosing handler on submit depend on condition */}
                             <form onSubmit={userExists ? handleLoginSubmit : handleSignUpSubmit}>
@@ -209,8 +241,9 @@ export default function Login() {
                                 </Button>
                             </form>
                         </Grid>
+                        )}
                         {/* Here it should be another condition for google aut is disabled or not */}
-                        {userExists && (
+                        {userExists && authData && (
                             <Grid item sx={{ mt: 4 }}>
                                 <GoogleButton variant='outlined' onClick={() => login()} sx={{ fontSize: '12px', fontWeight: 500 }}>
                                     Sign in with Google
