1b28015c1b21675128cf5a2944a5de87316e7c5b
Cesar Gonzalez <cegogo_6@hotmail.com>
2024-06-18 14:39:51 +0200

    Basic user info done

--- src/pages/users/UserProfile.tsx
+++ src/pages/users/UserProfile.tsx
@@ -92,90 +92,67 @@ export function UserProfile() {
 
     return (
         <Box sx={{ mt: '120px', p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-          <Box sx={{ width: '100%' }}>
-            <Card sx={{ borderRadius: '7px' }}>
-              <Box sx={{ p: '20px', borderBottom: '1px solid lightgray', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Typography variant="h6" sx={{ fontWeight: 600, color: '#1a3353f0' }}>My profile</Typography>
-              </Box>
-              <Box sx={{ p: '20px', borderBottom: '1px solid lightgray', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <Avatar src={userProfile.user_details.profile_pic || ''} alt="Profile Picture" sx={{ width: 120, height: 120, mr: '20px' }} />
-                <Box>
-                  <Typography variant="h4" sx={{ fontWeight: 600 }}>My name</Typography>
-                  <Typography variant="body1" sx={{ color: 'gray', mb: '10px' }}>Job title</Typography>
-                  <Box sx={{ backgroundColor: '#4f575b', color: 'white', px: '10px', py: '5px', borderRadius: '4px', display: 'inline-block' }}>
-                    <Typography variant="body2">{userProfile.role || '---'}</Typography>
-                  </Box>
-                </Box>
-              </Box>
-              <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Box sx={{ width: '32%' }}>
-                  <Typography variant="body2" className="title2">Email</Typography>
-                  <Typography variant="body1" className="title3">{userProfile.user_details.email || '---'}</Typography>
-                </Box>
-                <Box sx={{ width: '32%' }}>
-                  <Typography variant="body2" className="title2">Is Active</Typography>
-                  <Switch checked={userProfile.user_details.is_active} />
-                </Box>
-              </Box>
-              <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', mt: '15px' }}>
-                <Box sx={{ width: '32%' }}>
-                  <Typography variant="body2" className="title2">Role</Typography>
-                  <Typography variant="body1" sx={{ color: '#1E90FF', mt: '5%' }}>{userProfile.role || '---'}</Typography>
-                </Box>
-                <Box sx={{ width: '32%' }}>
-                  <Typography variant="body2" className="title2">Mobile Number</Typography>
-                  <Typography variant="body1" className="title3">{userProfile.phone || '---'}</Typography>
-                </Box>
-                <Box sx={{ width: '32%' }}>
-                  <Typography variant="body2" className="title2">Marketing Access</Typography>
-                  <Switch checked={userProfile.has_marketing_access} />
-                </Box>
-              </Box>
-              <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', mt: '15px' }}>
-                <Box sx={{ width: '34%' }}>
-                  <Typography variant="body2" className="title2">Sales Access</Typography>
-                  <Switch checked={userProfile.has_sales_access} />
-                </Box>
-                <Box sx={{ width: '32%' }}>
-                  <Typography variant="body2" className="title2">Date of joining</Typography>
-                  <Typography variant="body1" className="title3">{userProfile.date_of_joining || '---'}</Typography>
-                </Box>
-              </Box>
-              <Box sx={{ mt: '15px' }}>
-                <Box sx={{ p: '20px', borderBottom: '1px solid lightgray', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <Typography variant="h6" sx={{ fontWeight: 600, color: '#1a3353f0' }}>Address</Typography>
-                </Box>
-                <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <Box sx={{ width: '32%' }}>
-                    <Typography variant="body2" className="title2">Address Line</Typography>
-                    <Typography variant="body1" className="title3">{userProfile.address.address_line || '---'}</Typography>
-                  </Box>
-                  <Box sx={{ width: '32%' }}>
-                    <Typography variant="body2" className="title2">Street</Typography>
-                    <Typography variant="body1" className="title3">{userProfile.address.street || '---'}</Typography>
-                  </Box>
-                  <Box sx={{ width: '32%' }}>
-                    <Typography variant="body2" className="title2">City</Typography>
-                    <Typography variant="body1" className="title3">{userProfile.address.city || '---'}</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', mt: '15px' }}>
-                  <Box sx={{ width: '32%' }}>
-                    <Typography variant="body2" className="title2">Postcode</Typography>
-                    <Typography variant="body1" className="title3">{userProfile.address.postcode || '---'}</Typography>
-                  </Box>
-                  <Box sx={{ width: '32%' }}>
-                    <Typography variant="body2" className="title2">State</Typography>
-                    <Typography variant="body1" className="title3">{userProfile.address.state || '---'}</Typography>
-                  </Box>
-                  <Box sx={{ width: '32%' }}>
-                    <Typography variant="body2" className="title2">Country</Typography>
-                    <Typography variant="body1" className="title3">{userProfile.address.country || '---'}</Typography>
-                  </Box>
-                </Box>
-              </Box>
-            </Card>
-          </Box>
+            <Box sx={{ width: '100%' }}>
+                <Card sx={{ borderRadius: '7px' }}>
+                    <Box sx={{ p: '20px', borderBottom: '1px solid lightgray', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Typography style={{ fontWeight: 600, fontSize: '18px', color: '#1a3353f0'}}>My profile</Typography>
+                    </Box>
+                    <Box sx={{ p: '20px', borderBottom: '1px solid lightgray', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <Avatar src={userProfile.user_details.profile_pic || ''} alt="Profile Picture" sx={{ width: 160, height: 160, mr: '20px' }} />
+                        <Box>
+                            <Typography variant="h4" sx={{ fontWeight: 600 }}>My name</Typography>
+                            <Typography sx={{ fontSize: '22px', color: 'gray', mb: '10px' }}>Job title</Typography>
+                            <Box sx={{ backgroundColor: '#4f575b', color: 'white', px: '10px', py: '5px', borderRadius: '4px', display: 'inline-block' }}>
+                                <Typography variant="body2">{userProfile.role || '---'}</Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                    <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', mt: '15px' }}>
+                        <Box sx={{ width: '32%' }}>
+                            <Typography className="title2">Email</Typography>
+                            <Typography className="title3">{userProfile.user_details.email || '---'}</Typography>
+                        </Box>
+                        <Box sx={{ width: '32%' }}>
+                            <Typography className="title2">Mobile Number</Typography>
+                            <Typography className="title3">{userProfile.phone || '---'}</Typography>
+                        </Box>
+                        <Box sx={{ width: '32%' }}>
+                            <Typography className="title2">Date of joining</Typography>
+                            <Typography className="title3">{userProfile.date_of_joining || '---'}</Typography>
+                        </Box>
+                    </Box>
+                    <Box sx={{ mt: '15px' }}>
+                        <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <Box sx={{ width: '32%' }}>
+                                <Typography className="title2">Address Line</Typography>
+                                <Typography className="title3">{userProfile.address.address_line || '---'}</Typography>
+                            </Box>
+                            <Box sx={{ width: '32%' }}>
+                                <Typography className="title2">Street</Typography>
+                                <Typography className="title3">{userProfile.address.street || '---'}</Typography>
+                            </Box>
+                            <Box sx={{ width: '32%' }}>
+                                <Typography className="title2">City</Typography>
+                                <Typography className="title3">{userProfile.address.city || '---'}</Typography>
+                            </Box>
+                        </Box>
+                        <Box sx={{ p: '20px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', mt: '15px' }}>
+                            <Box sx={{ width: '32%' }}>
+                                <Typography className="title2">Postcode</Typography>
+                                <Typography className="title3">{userProfile.address.postcode || '---'}</Typography>
+                            </Box>
+                            <Box sx={{ width: '32%' }}>
+                                <Typography className="title2">State</Typography>
+                                <Typography className="title3">{userProfile.address.state || '---'}</Typography>
+                            </Box>
+                            <Box sx={{ width: '32%' }}>
+                                <Typography className="title2">Country</Typography>
+                                <Typography className="title3">{userProfile.address.country || '---'}</Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Card>
+            </Box>
         </Box>
-      );
-    }
+    );
+}
